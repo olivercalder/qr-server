95e137b7f8b70209f9600da9d0dcbb632c96d5bb
Oliver Calder <olivercalder@protonmail.ch>
2022-03-01 18:17:08 -0600

    Made Python path portable.

--- restart_qr.sh
+++ restart_qr.sh
@@ -1,4 +1,4 @@
 /usr/bin/ps -aux | /usr/bin/grep -v 'grep' | /usr/bin/grep 'qr_server.py' > /dev/null
 if [ $? -ne 0 ]; then
-    /usr/bin/tmux new-session -d -s qr_server /usr/local/bin/python3 /home/oac/qr/qr_server.py localhost 5000
+    /usr/bin/tmux new-session -d -s qr_server /usr/bin/env python3 /home/oac/qr/qr_server.py localhost 5000
 fi

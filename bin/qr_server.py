ea859c14003ce1efec4d00de04c33d83f6b41006
Oliver Calder <oliver@calder.dev>
2025-03-16 11:47:48 -0500

    bin/qr_server.py: add CLI and flask arguments for max content length
    Signed-off-by: Oliver Calder <oliver@calder.dev>

--- bin/qr_server.py
+++ bin/qr_server.py
@@ -6,11 +6,12 @@
 
     A simple server to generate QR codes for data following the / in the URL.
 '''
-import sys
 import argparse
 import flask
-import subprocess
 import io
+import os
+import subprocess
+import sys
 
 app = flask.Flask(__name__)
 
@@ -26,8 +27,12 @@ def render_path_as_qr(path):
     return flask.send_file(io_result, mimetype='image/png')
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser('A simple server to generate QR codes, built using Flask and qrencode.')
+    parser = argparse.ArgumentParser(
+            prog=os.path.basename(sys.argv[0]),
+            description='A simple server to generate QR codes, built using Flask and qrencode.')
     parser.add_argument('host', help='the host on which this application is running')
     parser.add_argument('port', type=int, help='the port on which this application is listening')
+    parser.add_argument('--max-content-length', type=int, default=100, help='the maximum content length in request bodies, in MiB')
     arguments = parser.parse_args()
+    app.config['MAX_CONTENT_LENGTH'] = arguments.max_content_length * 1024 * 1024
     app.run(host=arguments.host, port=arguments.port)
